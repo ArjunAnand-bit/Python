e4af55434119add5198c593d3c15815383eb0188
Arjun <arjunanand.sep24@gmail.com>
2024-10-13 12:16:53 +0530

    modified file

--- app.py
+++ app.py
@@ -1,16 +1,31 @@
 b = " Hello, World! "
-print(b[:11])
-print(b.upper())
-print(b.lower())
-print(b.strip())
-print(b.replace("H","Y"))
-print(b.split(","))
+print(b[:11])# type: ignore
+print(b.upper())# type: ignore
+print(b.lower())# type: ignore
+print(b.strip()) # type: ignore
+print(b.replace("H","Y"))# type: ignore
+print(b.split(","))# type: ignore
 
 
 
+#We can try using '''
+
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+
+for x in "Banana":
+ print(x) # type: ignore
+
+print(len(b)) # type: ignore
+
+txt = "The best things in life are free!"
+print("free" in txt) # type: ignore
 
 
-
+txt2 = "The best things in life are free!"
+print("expensive" not in txt) # type: ignore
 
 
 
@@ -100,4 +115,4 @@ print(b.split(","))
 a = "Hello"
 b = "World"
 
-print(a+",Mellow"+" "+b)
+print(a+",Mellow"+" "+b)# type: ignore
